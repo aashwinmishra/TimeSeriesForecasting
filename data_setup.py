4a830d5e5a58b95e0ad08216f02ad8b89656f8cb
Aashwin Mishra <62309130+aashwinmishra@users.noreply.github.com>
2024-09-18 23:52:22 -0700

    Update data_setup.py

--- data_setup.py
+++ data_setup.py
@@ -1,7 +1,7 @@
 import os
 import urllib.request
 import numpy as np
-import pandas as pd 
+import pandas as pd
 
 
 def get_datafile(url, data_dir, destination_file)-> None:
@@ -30,14 +30,6 @@ def get_data(data_file, index_col, target_col, new_target_col_name, ratio: float
   """
   Accesses datafile with time series data, selects the target column.
   Returns a tuple of x_train, x_val, y_train, y_val.
-  Args:
-    data_file: path to file with time series data
-    index_col: name of cloumn to order by
-    target_col: Name of target column
-    new_target_col_name: new name of target
-    ratio: Ratio for split between train and validation data.
-  Returns:
-    x_train, x_val, y_train, y_val
   """
   df = pd.read_csv(data_file,parse_dates=[index_col],index_col=[index_col])
   final_df = pd.DataFrame(df[target_col]).rename(columns={target_col: new_target_col_name})
@@ -46,3 +38,31 @@ def get_data(data_file, index_col, target_col, new_target_col_name, ratio: float
   N  = int(len(time_steps)*ratio)
   return target[:N], target[N:], time_steps[:N], time_steps[N:]
 
+
+def get_labeled_windows(x, window_size: int=7, horizon: int=1)->tuple:
+  """
+  Takes a sequence of values as a 1-D numpy array.
+  Returns a tuple of sliding windows and sliding horizons to be used as
+  inputs and targets for sequence modeling. Similar to 
+  tf.keras.preprocessing.timeseries_dataset_from_array().
+  Args:
+    x: sequence of values, 1-D numpy array.
+    window_size: length of sliding windows for the input features.
+    horizon: length of sliding windows for the target.
+  Returns:
+    tuple of 
+  """
+  windows = np.lib.stride_tricks.sliding_window_view(x[:-horizon], window_size)
+  horizons = np.lib.stride_tricks.sliding_window_view(x[window_size:], window_shape=horizon)
+
+  return windows, horizons
+
+
+def make_train_test_splits(windows, labels, test_split=0.2):
+  """
+  Splits the tuple of windowed inputs and targets into train and test/val tuples.
+  """
+  split_idx = 1 - int(len(windows)*test_split)
+  return windows[:split_idx], windows[split_idx:], labels[:split_idx], labels[split_idx:]
+
+  
