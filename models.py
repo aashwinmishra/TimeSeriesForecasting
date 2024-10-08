6da3224a9a3b61adc5281056a633fcbe94a85bf1
Aashwin Mishra <62309130+aashwinmishra@users.noreply.github.com>
2024-10-08 01:29:39 -0700

    Update models.py

--- models.py
+++ models.py
@@ -81,6 +81,43 @@ class Stack(tf.keras.layers.Layer):
     return inputs, forecast
     
 
+class NBeats(tf.keras.models.Model):
+  """
+  Uses the BasicBlock and Stack classes to define the N-BEATS model (generic), 
+  outlined in Oreshkin et al (2020).
+  """
+  def __init__(self, 
+               num_stacks: int,
+               num_blocks: int, 
+               input_size: int, 
+               theta_size: int, 
+               horizon: int, 
+               n_neurons: int, 
+               n_layers: int, 
+               **kwargs):
+    """
+    Initializes the model for N-BEATS.
+    Args:
+      num_stacks: Number of stacks in the model
+      num_blocks: Number of blocks constituting the stack
+      input_size: size of window to predict from
+      theta_size: dimensionality of the parameters for the bases
+      horizon: steps to predict for
+      n_neurons: neurons in the FC layers
+      n_layers: number of layers in the FC section of the block
+    """
+    super().__init__(**kwargs)
+    self.stacks = [Stack(num_blocks, input_size, theta_size, horizon, n_neurons, n_layers) for _ in range(num_stacks)]
+
+  def call(self, inputs):
+    forecast = []
+    for Stack in self.stacks:
+      x_hat, y_hat = Stack(inputs)
+      inputs = tfkl.subtract([inputs, x_hat])
+      forecast.append(y_hat)
+    forecast = tfkl.add(forecast)
+    return forecast
+
 
 def create_model_checkpoint(model_name, save_path: str="Experiments"):
   return tf.keras.callbacks.ModelCheckpoint(filepath=os.path.join(save_path, model_name+".keras"),
